ff9f67d7475d73d6e1e3e19b6935a3e34f47e66b
David Orriss Jr <codethought@Davids-Mac-Pro.local>
2017-02-05 20:02:53 -0800

    Initial commit

---
+++ src/main/java/com/dorriss/payroll/DatabaseLoader.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2015 the original author Greg Turnquist or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dorriss.payroll;
+
+/**
+ * @author David Orriss (fork)
+ * @author Greg Turnquist (original author)
+ */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+// tag::code[]
+@Component
+public class DatabaseLoader implements CommandLineRunner {
+
+    private final EmployeeRepository repository;
+
+    @Autowired
+    public DatabaseLoader(EmployeeRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        this.repository.save(new Employee("Frodo", "Baggins", "ring bearer"));
+        this.repository.save(new Employee("Bilbo", "Baggins","thief"));
+        this.repository.save(new Employee ("Gandalf", "The Grey","wizard"));
+    }
+}
+// end::code[]
